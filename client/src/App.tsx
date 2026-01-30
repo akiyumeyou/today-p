f90b25b316b72d7430401dc613c639a322b01284
akiko sato <satoaki@akikonoMacBook-Air.local>
2026-01-30 20:22:51 +0900

    Add URL parameter support for direct mood selection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,36 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
+import { useEffect } from "react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import MoodSelection from "@/pages/MoodSelection";
 import CharacterResult from "@/pages/CharacterResult";
 import NotFound from "@/pages/not-found";
+import { submitMood, MoodType } from "@/lib/api";
+
+// URLパラメータから直接結果画面へリダイレクト
+function MoodRedirect() {
+  const [, setLocation] = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const mood = params.get('mood') as MoodType | null;
+
+    if (mood && ['happy', 'normal', 'sad'].includes(mood)) {
+      // APIに送信
+      submitMood(mood).catch(console.error);
+      // 結果画面へリダイレクト
+      setLocation(`/result/${mood}`);
+    }
+  }, [setLocation]);
+
+  return <MoodSelection />;
+}
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={MoodSelection} />
+      <Route path="/" component={MoodRedirect} />
       <Route path="/result/:mood" component={CharacterResult} />
       <Route component={NotFound} />
     </Switch>
